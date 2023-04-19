99e36c98f268f8f547accccf56b40caf88bbd339
nndya <92634812+anindiasasi733@users.noreply.github.com>
2023-04-19 10:06:29 +0700

    Add files via upload

---
+++ kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Anindia Sasikirana</p>
+        <p>Nim : 312110268</p>
+        <p>Kelas : TI.21.A.2</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : anindiakirana733@gmail.com</p>
+        <p>No. Telp : 08987920542/p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
